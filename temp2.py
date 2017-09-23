b5e3e04809e78bc9e914a8aeed72ed241bbc3763
March0ns <31839553+March0ns@users.noreply.github.com>
2017-09-23 21:36:03 +0800

    Create temp2.py

---
+++ temp2.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import time
+
+def mv():
+    for i in range(120):
+        w.move(text1, 10, 0)
+        time.sleep(0.1);
+        w.update()
+
+
+
+root = Tk()
+w = Canvas(root, width=500, height=500)
+w.pack()
+text1 = w.create_text(0, 250, text='王硕')
+btn = Button(root, text='点击这里',command=mv)
+btn.pack();
+mainloop()
+
